42ddc0ce8a9af018069911bcaf34e618b304e8e6
skayliu <skay463@163.com>
2025-07-28 18:48:51 +0800

    add milvus database use operation

---
+++ src/main/java/io/kestra/plugin/milvus/DatabaseUse.java
@@ -0,0 +1,92 @@
+package io.kestra.plugin.milvus;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.runners.RunContext;
+import io.milvus.v2.client.MilvusClientV2;
+import io.milvus.v2.service.database.request.DescribeDatabaseReq;
+import io.milvus.v2.service.database.response.DescribeDatabaseResp;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@ToString
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Schema(title = "This operation change the database in use.")
+@Plugin(
+    examples = {
+      @Example(
+          title = "Send database change request to a Milvus instance.",
+          full = true,
+          code =
+              """
+                id: use_milvus_database
+                namespace: company.team
+
+                inputs:
+                  - id: database_name
+                    type: STRING
+
+                tasks:
+                  - id: database_use
+                    type: io.kestra.plugin.milvus.DatabaseUse
+                    url: "http://localhost:19530"
+                    databaseName: "{{ inputs.database_name }}"
+              """),
+      @Example(
+          title = "Send database change request to a Milvus clusters of Zilliz Cloud.",
+          full = true,
+          code =
+              """
+                id: use_milvus_cloud_database
+                namespace: company.team
+
+                inputs:
+                  - id: database_name
+                    type: STRING
+
+                tasks:
+                  - id: database_use
+                    type: io.kestra.plugin.milvus.DatabaseUse
+                    url: "https://cluster-id.serverless.cluster-region.cloud.zilliz.com"
+                    token: "{{ secret('MILIVUS_API_KEY') }}"
+                    databaseName: "{{ inputs.database_name }}"
+              """)
+    })
+public class DatabaseUse extends MilvusConnection implements RunnableTask<DatabaseUse.Output> {
+
+  @Schema(title = "The name of the target database.")
+  @PluginProperty(dynamic = true)
+  @NotBlank
+  private String databaseName;
+
+  @Override
+  public Output run(RunContext runContext) throws Exception {
+    MilvusClientV2 client = connect(runContext);
+
+    String renderedDbName = runContext.render(databaseName);
+
+    DescribeDatabaseResp descDBResp =
+        client.describeDatabase(DescribeDatabaseReq.builder().databaseName(renderedDbName).build());
+
+    runContext.logger().info("Database {} is being used.", descDBResp.getDatabaseName());
+
+    client.useDatabase(renderedDbName);
+
+    return Output.builder().success(true).build();
+  }
+
+  @Getter
+  @Builder
+  public static class Output implements io.kestra.core.models.tasks.Output {
+
+    @Schema(title = "Indicates whether the database used was successful.")
+    private Boolean success;
+  }
+}
